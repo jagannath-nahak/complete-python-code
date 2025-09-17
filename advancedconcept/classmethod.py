298db1594f8ab99e5e78df2ef8baccbcf233b546
Jagannath Nahak <jagannathnahak551@gmail.com>
2025-09-17 16:24:35 +0530

    alternative-constructor

--- advancedconcept/classmethod.py
+++ advancedconcept/classmethod.py
@@ -1,14 +1,27 @@
-class Animal:
-    species="Mammal"
+# class Animal:
+#     species="Mammal"
     
-    @classmethod
-    def get_species(cls):
-        return cls.species
+#     @classmethod
+#     def get_species(cls):
+#         return cls.species
     
-    @classmethod
-    def set_species(cls,new_species):
-        cls.species=new_species
+#     @classmethod
+#     def set_species(cls,new_species):
+#         cls.species=new_species
 
-print(Animal.get_species())
-Animal.set_species("Reptile")
-print(Animal.get_species())
+# print(Animal.get_species())
+# Animal.set_species("Reptile")
+# print(Animal.get_species())
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    @classmethod
+    def from_string(cls,data):
+        return cls(data.split("-")[0],data.split("-")[1])
+
+data="Alok-20"
+p1=Person.from_string(data)
+print(p1.name)
+print(p1.age)
