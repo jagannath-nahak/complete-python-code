faff3a2e49fe43b8e9a5e79e7987bf3187c25011
Jagannath Nahak <jagannathnahak551@gmail.com>
2025-09-20 09:28:06 +0530

    walrus-operator

--- advancedconcept/walrusoperator.py
+++ advancedconcept/walrusoperator.py
@@ -1,12 +1,32 @@
 # foods=list()
 
+#without walrus 
+
 # while True:
 #     food=input("what food do you want to eat?")
 #     if food=="quit":
 #         break
 #     foods.append(food)
 
-foods=list()
+# with walrus operator
+# foods=list()
 
-while(food:=input("what food you want to eat?"))!="quit":
-    foods.append(food)
+# while(food:=input("what food you want to eat?"))!="quit":
+#     foods.append(food)
+
+# without walrus
+# numbers=[1,2,3,4,5]
+
+# results=[x*2 for x in numbers if x*2>1]
+# print(results)
+
+# without walrus
+# results=[y for x in numbers if (y:=x*2)>5]
+# print(results)
+
+
+
+
+
+
+
